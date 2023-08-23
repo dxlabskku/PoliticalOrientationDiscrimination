3eaeefc90b2166c326adce0abb747a6330b17ef7
jungkyun-lee <126649723+jungkyun-lee@users.noreply.github.com>
2023-08-23 11:45:23 +0900

    Update binary-classification.py

--- code/binary-classification.py
+++ code/binary-classification.py
@@ -1,1 +1,21 @@
+import re
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm
+from math import log
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import KFold
+from xgboost import XGBClassifier
+from sklearn.metrics import classification_report
+from khaiii import KhaiiiApi
+from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.model_selection import GridSearchCV
+from imblearn.under_sampling import RandomUnderSampler
 
